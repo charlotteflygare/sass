675d4234094ef3c41b471c66a0ba48820594b09c
charlotteflygare <38113960+charlotteflygare@users.noreply.github.com>
2018-10-17 21:52:55 +0200

    Styled

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -5,56 +5,74 @@ class App extends React.Component {
   render() {
     return (
       <div>
+
+
+
         <div className="header">
           <img src="logo.svg" />
         </div>
+
         <div className="heroImage">
           <img src="./hero.jpg" />
+          </div>
 
-            <div className="container">
-              <div className="destinationContainer">
-                <div className="flyContainer">
-                <h4>Flyg från</h4>
-                  <div className="flyBorder">
-                    <form>
-                    <label>
-                    <input type="text" name="name" placeholder="Vart flyger du ifrån?"/>
-                    </label>
-                  </form>
+
+          <div className="container">
+            <div className="tab-menu">
+                  <div className="tab-menu-fly">
+                    <p>Flyg</p>
                   </div>
-
-                  <div className="link">
-                    <a>Stockholm-Arlanda ?</a></div>
+                  <div className="tab-menu-flyhotel">
+                    <p>Flyg & hotell</p>
+                  </div>
+                  <div className="tab-menu-rental">
+                    <p>Hyrbil</p>
+                  </div>
+                  <div className="tab-menu-hotel">
+                    <p>Hotell</p>
+                  </div>
                 </div>
 
-             <div className="flyContainer">
-             <h4>Flyg till</h4>
-             <div className="flyBorder">
+              <div className="destinationContainer">
 
-               <form>
-                 <label>
-                 <input type="text" name="name" placeholder="Var flyger du till?"/>
-                 </label>
-               </form>
-               </div>
+                <div className="flyContainer tab active" id="tab1">
+                  <h4>Flyg från</h4>
+                      <div className="flyBorder">
+                        <form>
+                          <label>
+                          <input type="text" name="name" placeholder="Vart flyger du ifrån?"/>
+                          </label>
+                        </form>
+                      </div>
+                      <div className="link">
+                        <a>Stockholm-Arlanda ?</a>
+                    </div>
+                </div>
 
-             </div>
+                     <div className="flyContainer">
+                      <h4>Flyg till</h4>
+                        <div className="flyBorder">
 
+                       <form>
+                         <label>
+                         <input type="text" name="name" placeholder="Var flyger du till?"/>
+                         </label>
+                       </form>
+                       </div>
 
+                     </div>
              </div>
 
             <div className="buttonContainer">
               <span>
               <button><p>Sök och beställ</p></button>
               </span>
-
             </div>
-
             </div>
+          </div>
 
 
-        </div>
-      </div>
+
     )
   }
 
