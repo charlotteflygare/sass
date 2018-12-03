af6d2f2644e7f86856597c13a31fdeb9cc9126e0
Charlotte Flygare <charlotte.flygare@gmail.com>
2018-12-03 14:02:47 +0100

    Eslint

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -1,77 +1,69 @@
 import React from "react"
 import "./app.scss"
+
 class App extends React.Component {
 
   render() {
     return (
       <div>
-
-
-
         <div className="header">
-          <img src="logo.svg" />
+          <img src="./logo.svg" alt="Norweigen logotype" />
         </div>
 
         <div className="heroImage">
-          <img src="./hero.jpg" />
+          <img src="./hero.jpg" alt="Norwiegen flight" />
+        </div>
+
+        <div className="container">
+          <div className="tab-menu">
+            <div className="tab-menu-fly">
+              <p>Flyg</p>
+            </div>
+            <div className="tab-menu-flyhotel">
+              <p>Flyg & hotell</p>
+            </div>
+            <div className="tab-menu-rental">
+              <p>Hyrbil</p>
+            </div>
+            <div className="tab-menu-hotel">
+              <p>Hotell</p>
+            </div>
           </div>
 
+          <div className="destinationContainer">
+            <div className="flyContainer tab active" id="tab1">
+              <h4>Flyg från</h4>
+              <div className="flyBorder">
+                <form>
+                  <label htmlFor="travel from">
+                    <input type="text" name="name" placeholder="Vart flyger du ifrån?" />
+                  </label>
+                </form>
+              </div>
+              <div className="link">
+                <a>Stockholm-Arlanda ?</a>
+              </div>
+            </div>
 
-          <div className="container">
-            <div className="tab-menu">
-                  <div className="tab-menu-fly">
-                    <p>Flyg</p>
-                  </div>
-                  <div className="tab-menu-flyhotel">
-                    <p>Flyg & hotell</p>
-                  </div>
-                  <div className="tab-menu-rental">
-                    <p>Hyrbil</p>
-                  </div>
-                  <div className="tab-menu-hotel">
-                    <p>Hotell</p>
-                  </div>
-                </div>
+            <div className="flyContainer">
+              <h4>Flyg till</h4>
+              <div className="flyBorder">
+                <form>
+                  <label htmlFor="destination">
+                    <input type="text" name="name" placeholder="Var flyger du till?" />
+                  </label>
+                </form>
+              </div>
+            </div>
+          </div>
 
-              <div className="destinationContainer">
-
-                <div className="flyContainer tab active" id="tab1">
-                  <h4>Flyg från</h4>
-                      <div className="flyBorder">
-                        <form>
-                          <label>
-                          <input type="text" name="name" placeholder="Vart flyger du ifrån?"/>
-                          </label>
-                        </form>
-                      </div>
-                      <div className="link">
-                        <a>Stockholm-Arlanda ?</a>
-                    </div>
-                </div>
-
-                     <div className="flyContainer">
-                      <h4>Flyg till</h4>
-                        <div className="flyBorder">
-
-                       <form>
-                         <label>
-                         <input type="text" name="name" placeholder="Var flyger du till?"/>
-                         </label>
-                       </form>
-                       </div>
-
-                     </div>
-             </div>
-
-            <div className="buttonContainer">
-              <span>
+          <div className="buttonContainer">
+            <span>
               <button><p>Sök och beställ</p></button>
-              </span>
-            </div>
-            </div>
+            </span>
           </div>
-
-
+        </div>
+      </div>
 
     )
   }
